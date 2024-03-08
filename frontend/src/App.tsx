d3756fed8866606ae18a27c1215e546ac0abac56
KS2499 <saraankannusami@gmail.com>
2024-03-09 05:04:27 +0800

    update main App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import AuthorForm from "./components/AuthorForm";
+import AuthorList from "./components/AuthorList";
+import { Author } from "./types/types";
+import "./App.css";
 
+const BASE_URL = "http://localhost:5000";
 
 function App() {
+  const [authors, setAuthors] = useState<Author[]>([]);
+  const [displayNames, setDisplayNames] = useState<string[]>([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const res = await axios.get(`${BASE_URL}/api/authors`);
+      setAuthors(res.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const filterAuthorsByName = (keyword: string): string[] => {
+    if (!authors || authors.length === 0 || keyword === "") {
+      return [];
+    }
+
+    const filteredAuthors = authors.filter((author) =>
+      author.name.toLowerCase().includes(keyword.toLowerCase())
+    );
+
+    const names = filteredAuthors.map((author) => author.name);
+
+    return names;
+  };
+
+  const handleSubmittedNameChange = (author: string) => {
+    setDisplayNames(filterAuthorsByName(author));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Assessment 1 App</h1>
+      <AuthorForm onSubmit={handleSubmittedNameChange} />
+      <AuthorList authorNames={displayNames} />
     </div>
   );
 }
