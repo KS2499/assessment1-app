608e03eb7867982b840724271c7df87232eea610
KS2499 <saraankannusami@gmail.com>
2024-03-09 15:23:04 +0800

    moved filter logic to backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,14 +6,14 @@ const app = express();
 app.use(cors());
 const PORT = 5000;
 
-app.get("/api/authors", async (req, res) => {
+app.get("/api/author_names", async (req, res) => {
   try {
     const response = await axios.get(
       "https://jsonplaceholder.typicode.com/users"
     );
     console.log(`got GET request`);
-    const authors = response.data;
-    res.json(authors);
+    const names = response.data.map((author) => author.name);
+    res.json(names);
   } catch (error) {
     console.error("Error fetching authors:", error);
     res.status(500).json({ error: "Failed to fetch authors" });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,12 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import AuthorForm from "./components/AuthorForm";
 import AuthorList from "./components/AuthorList";
-import { Author } from "./types/types";
 import "./App.css";
 
 const BASE_URL = "http://localhost:5000";
 
 function App() {
-  const [authors, setAuthors] = useState<Author[]>([]);
+  const [authorNames, setAuthorNames] = useState<string[]>([]);
   const [displayNames, setDisplayNames] = useState<string[]>([]);
 
   useEffect(() => {
@@ -17,23 +16,23 @@ function App() {
 
   const fetchData = async () => {
     try {
-      const res = await axios.get(`${BASE_URL}/api/authors`);
-      setAuthors(res.data);
+      const res = await axios.get(`${BASE_URL}/api/author_names`);
+      setAuthorNames(res.data);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
 
   const filterAuthorsByName = (keyword: string): string[] => {
-    if (!authors || authors.length === 0 || keyword === "") {
+    if (!authorNames || authorNames.length === 0 || keyword === "") {
       return [];
     }
 
-    const filteredAuthors = authors.filter((author) =>
-      author.name.toLowerCase().includes(keyword.toLowerCase())
+    const filteredAuthors = authorNames.filter((authorName) =>
+      authorName.toLowerCase().includes(keyword.toLowerCase())
     );
 
-    const names = filteredAuthors.map((author) => author.name);
+    const names = filteredAuthors.map((authorName) => authorName);
 
     return names;
   };
